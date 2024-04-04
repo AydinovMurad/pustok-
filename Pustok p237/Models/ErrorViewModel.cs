e5cbf577d2d7fde3e52cf8af7832f5ed40b7e6eb
AydinovMurad <muradmay@code.edu.az>
2024-04-04 15:01:50 +0400

    front to back

---
+++ Pustok p237/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pustok_p237.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
